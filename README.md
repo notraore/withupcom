923c81314c9b41374e5af1875950ba29fe5730c5
Nobila <notraore@student.42.fr>
2021-11-12 11:11:38 +0100

    Website refresh

---
+++ README.md
@@ -0,0 +1,10 @@
+# nodejs-react-socket.io-boilerplate
+Boilerplate for Nodejs and React connection using socket.io
+
+The backend and frontend is running on separete ports
+
+Run the Nodejs server by executing ```npm start``` inside nodejs-server folder.
+
+Run the React client by executing ```npm start``` inside react-client folder.
+
+Server runson port 3000 and the client on 4000
--- nodejs-server/server.js
+++ nodejs-server/server.js
@@ -12,7 +12,9 @@ const { manangeInstanceId } = require("./ec2_startstopinstances.js");
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
-const ID = "i-04f63950842ad7e2b";
+var ID = "";
+var AllID = [];
+var usedID = [];
 
 server.listen(port, () =>
 	console.log(`Nodejs Server listening on port :${port}!`)
@@ -21,10 +23,51 @@ server.listen(port, () =>
 io.on("connection", function (socket) {
 	const previousCode = null;
 	console.log("a user is connected");
-	const interval = setInterval(() => {
-		checkStatus(socket, interval);
-	}, 5000);
+	const inter = setInterval(() => {
+		getInstances(inter);
+	}, 1000);
+	console.log(AllID + "NOBILA");
+	// const interval = setInterval(() => {
+	// checkStatus(socket, interval);
+	// }, 5000);
 });
+
+const compareIDArray = () => {
+	if (usedID.length == 0) {
+		usedID.push(AllID[0]);
+		ID = usedID[0];
+		console.log(ID + " ID");
+	} else if (usedID.length != 0) {
+		for (var i = 0; i < AllID.length; i++) {
+			var test = usedID.indexOf(AllID[i]);
+			console.log(test);
+		}
+	}
+};
+
+const getInstances = (inter) => {
+	exec(
+		`aws ec2 describe-instances --filters "Name=instance-type,Values=g4dn.4xlarge" --query "Reservations[].Instances[].InstanceId"`,
+		(error, stdout, stderr) => {
+			if (error) {
+				console.log(`error: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				console.log(`stderr: ${stderr}`);
+				return;
+			}
+			// console.log(JSON.parse(stdout));
+			AllID = JSON.parse(stdout);
+			if (AllID.length != 0) {
+				clearInterval(inter);
+				console.log(AllID.length);
+				compareIDArray();
+			}
+			// console.log(AllID);
+		}
+	);
+};
 
 const checkStatus = (socket, interval) => {
 	manangeInstanceId("START", ID, function (data) {
